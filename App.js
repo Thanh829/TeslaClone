46de9fce127566739e7f686e1785e4bc32f18928
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2021-10-13 09:42:44 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItem';
+import CarList from './components/CarList';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <CarList />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
